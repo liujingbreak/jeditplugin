a9f03a9aa4135bedb75a35df64cb79c67697b8de
LJ <lji@tradeshift.com>
2013-11-14 00:49:34 -0800

    new parser applied

--- src/org/liujing/parser/CSSParser.java
+++ src/org/liujing/parser/CSSParser.java
@@ -16,10 +16,11 @@ import org.liujing.tool.JSHandler;
 import errorlist.*;
 import sidekick.*;
 import org.liujing.ironsword.grammar.*;
+import org.liujing.antlr.parser.LessCssParserListener;
 
 public class CSSParser extends SideKickParser{
     private Logger log = Logger.getLogger(CSSParser.class.getName());
-
+    
 	static Pattern docKeywordPat = Pattern.compile("[a-zA-Z0-9_$'\\.\"]+(?: [a-zA-Z0-9_$'\\.\"]+)*", Pattern.MULTILINE );
 
 	public CSSParser(String name){
@@ -36,31 +37,26 @@ public class CSSParser extends SideKickParser{
 	}
 
 	protected SideKickParsedData start(org.gjt.sp.jedit.Buffer buffer, DefaultErrorSource errorSource){
-	    ANTLRStringStream in = new ANTLRStringStream(buffer.getText(0,buffer.getLength()));
-        MyCSSLexer lexer = new MyCSSLexer(in);
-        RemovableTokenStream tokens = new RemovableTokenStream(lexer);
-        MyCSSParser p = new MyCSSParser(tokens);
-        SideKickParsedData data = new SideKickParsedData(buffer.getName());
-        AntlrGrammarHandler h = new AntlrGrammarHandler();
-        p.setHandler(h);
-        try{
-            p.cssfile(buffer.getName());
-            parseNodeToSidekick(data.root, h);
-        }catch(RecognitionException e){
-            errorSource.addError(ErrorSource.ERROR, buffer.getPath(), e.line, ((CommonToken)e.token).getStartIndex(), ((CommonToken)e.token).getStopIndex()+1, e.getMessage());
+	  SideKickParsedData data = null;
+	try{
+	    	
+	    	GrammarNode node = LessCssParserListener.parseText(buffer.getText(0,buffer.getLength()), buffer.getName());
+	        data = new SideKickParsedData(buffer.getName());
+	    	parseNodeToSidekick(data.root, node);
+        }catch(Exception e){
+        	log.log(Level.WARNING, "", e);
+          //errorSource.addError(ErrorSource.ERROR, buffer.getPath(), e.line, ((CommonToken)e.token).getStartIndex(), ((CommonToken)e.token).getStopIndex()+1, e.getMessage());
         }
         return data;
-	}
-    
-	private void parseNodeToSidekick(DefaultMutableTreeNode uiNode, AntlrGrammarHandler h){
-	    GrammarNode gnode = h.getNode();
-	    parseNodeToSidekick(uiNode, gnode);
 	}
 	
 	private void parseNodeToSidekick(DefaultMutableTreeNode uiNode, GrammarNode gnode){
 	    for(GrammarNode cg : gnode.getChildren()){
 	        if(cg.isType("doc")){
-	            createUINode_doc(uiNode, cg);
+	        	for(GrammarNode doc_node : cg.getChildren()){
+	        		if(doc_node.isType("desc"))
+	        			createUINode_doc(uiNode, doc_node, cg.getStartOffset(), cg.getEndOffset()); // desc node's offset is not correct, cuz' it is nested parser tree
+	        	}
 	        }else if(cg.isType("unit")){
 	            createUINode_green(uiNode, cg);
 	        }else if(cg.isType("rule")){
@@ -70,14 +66,14 @@ public class CSSParser extends SideKickParser{
 	}
 	
 	private void createUINode_doc(DefaultMutableTreeNode parent, 
-	    GrammarNode node)
+	    GrammarNode node, int startOffset, int endOffset)
 	{
 	    DefaultMutableTreeNode uiNode = new DefaultMutableTreeNode();
 	    Matcher m = docKeywordPat.matcher(node.getName());
 	    if(m.find()){
 	        YellowSquareNode icon = new YellowSquareNode(m.group(), node.getName());
-	        icon.setStartOffset(node.getStartOffset());
-	        icon.setEndOffset(node.getEndOffset());
+	        icon.setStartOffset(startOffset);
+	        icon.setEndOffset(endOffset);
 	        uiNode.setUserObject(icon);
 	        parent.add( uiNode);
 	    }
@@ -92,6 +88,7 @@ public class CSSParser extends SideKickParser{
 	    icon.setEndOffset(node.getEndOffset());
 	    uiNode.setUserObject(icon);
 	    parent.add(uiNode);
+	    parseNodeToSidekick(uiNode, node);
 	}
 	
 	private void createUINode_rule(DefaultMutableTreeNode parent,
